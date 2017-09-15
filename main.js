e1c268608c2f4bc7ad2d107536e8931e61ac9fe5
kcontreras01 <kiara@Kiaras-MacBook-Pro.local>
2017-09-15 14:04:17 -0400

    Class is toggling

--- main.js
+++ main.js
@@ -1,50 +1,46 @@
-var jq = $(function(){
+//make qwerty change with imgs
 
-// var scenes = 
-// 	['start', 'Qwerty is awoken by a rumbling belly', "", ["Wake my human", "end", "Look under the couch", 'couch']],
-// 	['couch', 'Qwerty gets on her belly and crawls under the couch. She finds a suspicious fuzzy cookie.', "", ["Eat the cookie", "sick", "Look under the table", 'table']]],
-// 	['table', 'Qwerty avoids the fuzzy cookie and makes her way to the table. The human is sitting there.', "", ["Look under the table", "end", "Avoid human", 'food']]],
-// 	['food', 'Qwerty makes her way to the closet where her food is kept. The human has left it open', "", ["Go in the closet", "win", "Sit outside", 'end']],
-// 	['end', 'Qwerty is in trouble and gets no food', "", 'start'],
-
+$(function(){
+// var gameStart = new Audio('ChibiNinja.mp3');
+// gameStart.play();
 
 var scenes = [
 	{
 		id: 1,
 		name: 'Start',
-		text: 'Qwerty is awoken by a rumbling belly',
+		text: "Qwerty is awoken by a rumbling belly. As she looks at the clock it's currently 5am. She has two options, wake up the human or search around the apartment.",
 		background: './img/livingroom.jpg',
 		choice: [{
 			answer: "Wake my human",
 			scene: "End",
 			},
 			{
-			answer: "Look under the couch",
+			answer: "Search the apartment",
 			scene: 'Couch',
 		}]  
 	},
 	{
 		id: 2,
 		name: 'Couch',
-		text: 'Qwerty gets on her belly and crawls under the couch. She finds a suspicious fuzzy cookie.',
+		text: 'Qwerty gets up and looks around. She smells underneath the couch and finds a suspicious fuzzy cookie. How long has this been here?',
 		background: '',
 		choice: [{
 			answer: "Eat the cookie",
 			scene: "Sick",
 			},
 			{
-			answer: "Look under the table",
+			answer: "Keep on searching",
 			scene: 'Table',
 		}]  
 	},
 	{
 		id: 3,
 		name: 'Table',
-		text: 'Qwerty avoids the fuzzy cookie and makes her way to the table. The human is sitting there.',
+		text: 'Qwerty avoids the fuzzy cookie and makes her way to the table. The human is sitting there drinking hot bean water. From here she can see food fell under the table.',
 		background: '',
 		choice: [{
 			answer: "Look under the table",
-			scene: "End",
+			scene: "Pizza",
 			},
 			{
 			answer: "Avoid human.",
@@ -53,66 +49,200 @@ var scenes = [
 	},
 	{
 		id: 4,
-		name: 'Food',
-		text: 'Qwerty makes her way to the closet where her food is kept. The human has left it open',
-		background: '',
+		name: 'Pizza',
+		text: 'Qwerty sneaks under the table quietly and finds some pizza. Yum! She gobbles it up but still wants more.',
 		choice: [{
-			answer: "Go in the closet",
+			answer: "Ask my human",
+			scene: "End",
+			},
+			{
+			answer: "Keep searching",
+			scene: 'Food',
+		}]  
+	},
+	{
+		id: 5,
+		name: 'Food',
+		text: 'Qwerty makes her way to the closet where her food is kept. The door is slightly ajar.',
+		background: './img/closet.jpg',
+		choice: [{
+			answer: "Paw at the door",
 			scene: "Win",
 			},
 			{
-			answer: "Sit outside",
+			answer: "Meow loudly",
 			scene: 'End',
 		}]  
 	},
 		{
-		id: 5,
+		id: 6,
 		name: 'Sick',
 		text: 'Qwerty eats the fuzzy cookie and gets sick! Qwerty gets in trouble and the human has to take you to the vet. Game Over.',
-		background: '',  
+		background: '', 
+		choice: [{
+			answer: "Restart",
+			scene: "Start",
+			}],  
 	},
 		{
-		id: 6,
-		name: 'End',
-		text: "The",
-		background: '',  
-	},
-	{
 		id: 7,
+		name: 'End',
+		text: "The human catches you and you're in trouble! Game Over",
+		background: '',
+		choice: [{
+			answer: "Restart",
+			scene: "Start",
+			}],  
+		},
+	{
+		id: 8,
 		name: 'Win',
 		text: 'Qwerty gets her fill of food and falls asleep again. What an awesome day.',
 		background: '',  
+		choice: [{
+			answer: "Restart",
+			scene: "Start",
+			}],
 	},
 ]
 
-
 function addQwerty(){
 	//add character to page
+	 // var qwerty = $('<div class="qwerty"></div>');
+  //  document.body.append(qwerty);
 	var qwerty = document.createElement('div');
 	qwerty.classList.add('qwerty');
 	document.body.append(qwerty);
 
+	setInterval(function(){
+    $(qwerty).toggleClass("stand");
+	}, 500);
 }
+
 addQwerty();
 
-function makeScene(){
-		//look through array and make scene
-		// $('scenes').each(function(){
-	scenes.forEach(function(elm, i){
-		// document.body.append(elm.text); 
-		$('body').append(elm.text);
-			// text.classList('p');
-		elm.choice.filter(function(elm, i){
-			// $('scenes').find('choice').each(function(elm, i){
-		// document.body.append(elm.answer); adds choices to page outside of button
-		// var button1 = document.getElementById('button1'); 
-		// var button1 = $(this).get(button1);
-		//button1.append(elm.answer);
-		//button2.append(elm.answer);
-
-		})
-	})
+//this function adds a div, appends it to the scene, makes that scene active, and creates 
+// answer buttons with their text inside.
+function makeScene3(sceneName){
+	$(".active-scene").remove();
+	var myScene = getSceneByName(sceneName);
+	var $scene = $(".scene");
+	var $activeScene = $("<div></div")
+													.appendTo($scene)
+													.addClass("active-scene")
+													// .css("background","url('myScene.background')")
+	$("<p></p>").appendTo($activeScene).text(myScene.text);
+	var answerObjs = getAnswersBySceneName2(sceneName);
+	for (var i in answerObjs){
+		var answerObj = answerObjs[i];
+		var $button = $("<button></button>")
+											.appendTo($activeScene)
+											.attr("next-scene", answerObj.scene)
+											.text(answerObj.answer)									
+											.click(sceneClick);
+		// $(answerObj).addClass('p');
+	}
 }
+
+//this function targets what has been clicked and gives it the attribute next-scene - 
+// moves scenes forward
+function sceneClick(){
+	var $button = $(this);
+	var nextScene = $button.attr("next-scene");
+	makeScene3(nextScene);
+};
+
+
+function chooseFirstScene(){
+	var initScene = scenes[0];
+	console.log(initScene.text);
+	//grab the choice array
+	var choiceArray = initScene.choice;
+	//iterate over choice and display answers
+	for (var i in choiceArray){
+		 var choiceObj = choiceArray[i];
+		 console.log(choiceObj.answer);
+	}
+	//console.log(initScene['text']);
+}
+
+
+//connects scene answers to next scene with same name
+function getSceneByName(sceneName){
+	var foundArray = scenes.filter(function(elem){
+		if (elem['name'] === sceneName) {
+			return true;
+		} else {
+			return false;
+		}
+	});
+
+	var foundScene = foundArray[0];
+	return foundScene;
+}
+
+//returns array of answer objects
+function getAnswersBySceneName2(sceneName){
+	var sceneObj = getSceneByName(sceneName);
+	var result = [];
+	for (var i in sceneObj.choice){
+		var choiceElem = sceneObj.choice[i];
+		result.push(choiceElem);
+	}
+	return result;
+	}
+
+makeScene3('Start');
+
+});
+
+//returns only array of strings OLD OLD OLD
+// $.fn.getAnswersBySceneName = function (sceneName){
+// 	var sceneObj = $.fn.getSceneByName(sceneName);
+// 	var result = [];
+// 	for (var i in sceneObj.choice){
+// 		var choiceElem = sceneObj.choice[i];
+// 		result.push(choiceElem.answer);
+// 	}
+// 	return result;
+	
+// }
+
+// function addChoices(){
+// 	// show scenes on screen
+// 	scenes.forEach(function(elm, i) {
+// 		// elm.text.forEach(function(elm, i){
+// 			// return scenes.text;
+// 			return scenes[i].text;
+// 			})
+// 		// })
+// 	}
+
+// var scenes = 
+// 	['start', 'Qwerty is awoken by a rumbling belly', "", ["Wake my human", "end", "Look under the couch", 'couch']],
+// 	['couch', 'Qwerty gets on her belly and crawls under the couch. She finds a suspicious fuzzy cookie.', "", ["Eat the cookie", "sick", "Look under the table", 'table']]],
+// 	['table', 'Qwerty avoids the fuzzy cookie and makes her way to the table. The human is sitting there.', "", ["Look under the table", "end", "Avoid human", 'food']]],
+// 	['food', 'Qwerty makes her way to the closet where her food is kept. The human has left it open', "", ["Go in the closet", "win", "Sit outside", 'end']],
+// 	['end', 'Qwerty is in trouble and gets no food', "", 'start'],
+
+// function makeScene(){
+// 		//look through array and make scene
+// 		// $('scenes').each(function(){
+// 	scenes.forEach(function(elm, i){
+// 		// document.body.append(elm.text); 
+// 		$('body').append(elm.text);
+// 			// text.classList('p');
+// 		elm.choice.filter(function(elm, i){
+// 			// $('scenes').find('choice').each(function(elm, i){
+// 		// document.body.append(elm.answer); adds choices to page outside of button
+// 		// var button1 = document.getElementById('button1'); 
+// 		// var button1 = $(this).get(button1);
+// 		//button1.append(elm.answer);
+// 		//button2.append(elm.answer);
+
+// 		})
+// 	})
+// }
 //makeScene();
 
 //THIS WORKS BUT THE BUTTONS DON"T HAVE ENOUGH DATA OLD OLD OLD
@@ -128,106 +258,3 @@ function makeScene(){
 // 		$("<button></button>").appendTo($activeScene).text(answer);
 // 	}
 // }
-
-
-$.fn.makeScene3 = function(sceneName){
-	$(".active-scene").remove();
-	var $scene = $(".scene");
-	var $activeScene = $("<div></div")
-													.appendTo($scene)
-													.addClass("active-scene")
-													.text("Active Scene");
-	var myScene = $.fn.getSceneByName(sceneName);
-	$("<h1></h1>").appendTo($activeScene).text(myScene.text);
-	var answerObjs = $.fn.getAnswersBySceneName2(sceneName);
-	for (var i in answerObjs){
-		var answerObj = answerObjs[i];
-		var $button = $("<button></button>")
-											.appendTo($activeScene)
-											.attr("next-scene", answerObj.scene)
-											.text(answerObj.answer)
-											.click(sceneClick);
-		
-	}
-}
-
-function sceneClick(){
-	var $button = $(this);
-	var nextScene = $button.attr("next-scene");
-	$.fn.makeScene3(nextScene);
-};
-
-$.fn.chooseFirstScene = function (){
-	var initScene = scenes[0];
-	console.log(initScene.text);
-	//grab the choice array
-	var choiceArray = initScene.choice;
-	//iterate over choice and display answers
-	for (var i in choiceArray){
-		 var choiceObj = choiceArray[i];
-		 console.log(choiceObj.answer);
-	}
-	//console.log(initScene['text']);
-}
-
-
-$.fn.getSceneByName = function (sceneName){
-	var foundArray = scenes.filter(function(elem){
-		if (elem['name'] === sceneName) {
-			return true;
-		} else {
-			return false;
-		}
-	});
-
-	var foundScene = foundArray[0];
-	return foundScene;
-}
-
-//returns only array of strings OLD OLD OLD
-// $.fn.getAnswersBySceneName = function (sceneName){
-// 	var sceneObj = $.fn.getSceneByName(sceneName);
-// 	var result = [];
-// 	for (var i in sceneObj.choice){
-// 		var choiceElem = sceneObj.choice[i];
-// 		result.push(choiceElem.answer);
-// 	}
-// 	return result;
-	
-// }
-
-//returns array of answer objects
-$.fn.getAnswersBySceneName2 = function (sceneName){
-	var sceneObj = $.fn.getSceneByName(sceneName);
-	var result = [];
-	for (var i in sceneObj.choice){
-		var choiceElem = sceneObj.choice[i];
-		result.push(choiceElem);
-	}
-	return result;
-	
-}
-
-
-// function addChoices(){
-// 	// show scenes on screen
-// 	scenes.forEach(function(elm, i) {
-// 		// elm.text.forEach(function(elm, i){
-// 			// return scenes.text;
-// 			return scenes[i].text;
-// 			})
-// 		// })
-// 	}
-
-// function nextScene(){
-
-// }	
-})
-
-
-// nextScene()
-// scene[3]
-
-// render()
-//
-
